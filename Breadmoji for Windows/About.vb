57d57e48b01809e09fa4bea35704e27fc95dfab7
johnb-xp <johnbilkey@protonmail.com>
2020-08-17 00:59:59 -0500

    Update Breadmoji for Windows to 1.6

--- Breadmoji for Windows/About.vb
+++ Breadmoji for Windows/About.vb
@@ -2,4 +2,8 @@
     Public Function GetVersion()
         Return Label3.Text
     End Function
+
+    Private Sub Label2_Click(sender As Object, e As EventArgs) Handles Label2.Click
+
+    End Sub
 End Class
